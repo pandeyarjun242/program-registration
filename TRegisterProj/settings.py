e2e4d879ccbf10774149ee0bf9522c01cf45d3b3
arjunpandey2003 <arjunpandey2003@gmail.com>
2020-05-23 17:32:22 +0530

    Settings

--- TRegisterProj/settings.py
+++ TRegisterProj/settings.py
@@ -58,7 +58,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'pandeyarjun.242@gmail.com'
 EMAIL_HOST_PASSWORD = 'arjun2003'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
