d0c9adf04a8f82eba5aadede0a7ab2b24d186648
arjunpandey2003 <arjunpandey2003@gmail.com>
2020-05-23 19:03:56 +0530

    Editting

--- account/views.py
+++ account/views.py
@@ -6,6 +6,8 @@ from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.core.validators import validate_email
 from django.core.mail import EmailMessage
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
 class Home(APIView):
     def get(self,request):
@@ -41,11 +43,15 @@ class Signup(APIView):
             message = request.POST['message']
             mobile = request.POST['mobile']
             profile = Player(name = name, email = email, mobile = mobile, age = 15, description =message, cityPreference = cityPreference)
-            emailm = EmailMessage('Registration Complete!',"Hi "+name+"\n\nWe are thrilled to let you know that you have been registered into India's first Trans Football Program. We have a lot of exciting stuff coming up! Stay tuned for more and let's rid the football ground of cis-sexist biases together.\n\nThe program is the brainchild of ScoutMe, The Keshav Suri Foundation and Bhaichung Bhutia Football Schools. Be sure to give us your support on social media \n\nBest,\nArjun Pandey\nFounder at ScoutMe", to=[str(email)])
-            emailm.send()
-            newEmail = EmailMessage('Registration - '+name, "\nEmail - "+email+"\nMobile- "+mobile +"\nMessage - " + message, to = ["arjun@scoutme.in"])
-            newEmail.send()
-            profile.save()
+            finm = "Hi "+name+"\n\nWe are thrilled to let you know that you have been registered into India's first Trans Football Program. We have a lot of exciting stuff coming up! Stay tuned for more and let's rid the football ground of cis-sexist biases together.\n\nThe program is the brainchild of ScoutMe, The Keshav Suri Foundation and Bhaichung Bhutia Football Schools. Be sure to give us your support on social media \n\nBest,\nArjun Pandey\nFounder at ScoutMe"
+            # emailm = EmailMessage('Registration Complete!',"Hi "+name+"\n\nWe are thrilled to let you know that you have been registered into India's first Trans Football Program. We have a lot of exciting stuff coming up! Stay tuned for more and let's rid the football ground of cis-sexist biases together.\n\nThe program is the brainchild of ScoutMe, The Keshav Suri Foundation and Bhaichung Bhutia Football Schools. Be sure to give us your support on social media \n\nBest,\nArjun Pandey\nFounder at ScoutMe", to=[str(email)])
+            # emailm.send()
+            # newEmail = EmailMessage('Registration - '+name, "\nEmail - "+email+"\nMobile- "+mobile +"\nMessage - " + message, to = ["arjun@scoutme.in"])
+            # newEmail.send()
+            # profile.save()
+            email_from = settings.EMAIL_HOST_USER
+            send_mail('Registration Complete!',finm, email_from,[str(email),])
+            send_email('Registration - '+name, "\nEmail - "+email+"\nMobile- "+mobile +"\nMessage - " + message,email_from,["arjun@scoutme.in,"])
             return render(request, 'aftersignup.html',{"error":"Your account has been created!"})
 class Contact(APIView):
     def get(self, request):
