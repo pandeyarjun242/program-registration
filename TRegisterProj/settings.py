a8daaae8ac85d57d78e7e77f36ad37ee9f0b9d75
arjunpandey2003 <arjunpandey2003@gmail.com>
2020-05-23 18:00:38 +0530

    Hello

--- TRegisterProj/settings.py
+++ TRegisterProj/settings.py
@@ -60,7 +60,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'pandeyarjun.242@gmail.com'
 EMAIL_HOST_PASSWORD = 'arjun2003'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
 TEMPLATES = [
